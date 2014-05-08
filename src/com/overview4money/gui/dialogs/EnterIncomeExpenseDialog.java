85e9cb687b34a248620cf83690cd84499765f3a7
Zordan <Zordan@Zordan-PC>
2014-05-08 23:15:35 +0200

    Income\Expense dialog zavrsen... Ostaje da se napravi dodavanje Rekorda kao i ubacivanje liste unutar auto list komponente.

--- src/com/overview4money/gui/dialogs/EnterIncomeExpenseDialog.java
+++ src/com/overview4money/gui/dialogs/EnterIncomeExpenseDialog.java
@@ -15,7 +15,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import com.overview4money.R;
@@ -55,6 +58,11 @@ public class EnterIncomeExpenseDialog extends DialogFragment {
 	this.m_incomeRadioButton = (RadioButton)dialogView.findViewById(R.id.incomeRadioButton);
 	this.m_expenseRadioButton.setChecked(true);
 	
+	//**** Get date picker. ****
+	this.m_datePicker = (DatePicker)dialogView.findViewById(R.id.inputDatePicker);
+	this.m_valueEditText = (EditText)dialogView.findViewById(R.id.valueEditText);
+	this.m_descriptionAutoTextView = (AutoCompleteTextView)dialogView.findViewById(R.id.descAutoCompleteTextView);
+	
 	TextView enterType = (TextView)dialogView.findViewById(R.id.enterTypeTV);
 	enterType.setText(enterType.getText() + ":");
 	TextView enterDate = (TextView)dialogView.findViewById(R.id.dateTV);
@@ -92,4 +100,8 @@ public class EnterIncomeExpenseDialog extends DialogFragment {
     private Button m_okButton, m_backButton;
     /** Expense and income radio buttons. Both are added to the radio group. */
     private RadioButton m_incomeRadioButton, m_expenseRadioButton;
+    /** Date picker, value text field and description text field necessary for money record creation. */
+    private DatePicker m_datePicker;
+    private EditText m_valueEditText;
+    private AutoCompleteTextView m_descriptionAutoTextView;
 }
